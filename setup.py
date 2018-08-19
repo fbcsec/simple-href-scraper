f2a1112bc41ab88ef1628a6d822fcfd409f848b4
fbcsec <fbc@disroot.org>
2018-08-18 22:43:50 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='Simple Scraper',
+      version='0.1.0',
+      author='fbcsec',
+      author_email='fbc@disroot.org',
+      scripts=['simple_scraper.py'],
+      install_requires=['bs4'])
